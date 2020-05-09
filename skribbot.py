04698566c911bd6446b60542fc3f764ead4effc8
Sergio Barril <sergiobarrilpizarro@gmail.com>
2020-05-09 13:48:54 +0200

    Code cleanup

--- skribbot.py
+++ skribbot.py
@@ -30,56 +30,72 @@ class Skribbot(discord.Client):
 
     async def on_message(self, message):
         if message.author == self.user:
-            return                
+            return
         
         if message.content == '.ready':
-            response = None
-            
-            # FIRST MESSAGE
-            if self.pinturillo is None:
-                self.pinturillo = Pinturillo()
-                # self.addReady(message.author)
-                response = "Venga va, ¿quién más se apunta? ¡Ya hay 1/4! @everyone"
+            self.messageReady(message)
 
-            # IF NOT ADDED ALREADY
-            elif self.addReady(message.author):
-                response = "¡Perfecto, {}!".format(message.author.mention)
-
-                if len(self.readyList) >= 2:
-                    response += " ¡Somos suficientes para que no dé pena!"
-                    if not self.pinturillo.URL:
-                        self.pinturillo.URL = "Loading"
-                        await message.channel.send("Cargando sala...")
-                        self.pinturillo.run()
-                        response += "Aquí tenéis el enlace: {}".format(self.pinturillo.URL)
-                    
-                else:
-                    response += " Ya somos {}/4".format(len(self.readyList))
-            await message.channel.send(response)
+        elif message.content == '.unready':
+            self.messageUnready(message)
         
+        elif message.content == '.link':
+            self.messageLink(message)
 
-        if message.content == '.unready':
-            if message.author in self.readyList:
-                self.readyList.remove(message.author)
-                response = "Terrible, {}. Eso es porque tienes miedo...".format(message.author.name)
+        elif message.content == '.readyList':
+            self.messageReadyList(message)       
+                        
+
+    def messageReady(self, message):
+        """
+        Handles the action if the message received was .ready
+        """
+        response = ""
+      
+        # FIRST MESSAGE
+        if self.pinturillo is None:
+            self.pinturillo = Pinturillo()
+            # self.addReady(message.author)
+            response = "Venga va, ¿quién más se apunta? ¡Ya hay 1/4! @everyone"
+
+        # IF NOT ADDED ALREADY
+        elif self.addReady(message.author):
+            response = "¡Perfecto, {}!".format(message.author.mention)
+
+            if len(self.readyList) >= 2:
+                response += " ¡Somos suficientes para que no dé pena!"
+                if not self.pinturillo.URL:
+                    self.pinturillo.URL = "Loading"
+                    await message.channel.send("Cargando sala...")
+                    self.pinturillo.run()
+                    response += "Aquí tenéis el enlace: {}".format(self.pinturillo.URL)
+                
             else:
-                response = "Pero si no habías hecho ready, so merluzo."
-            await message.channel.send(response)
+                response += " Ya somos {}/4".format(len(self.readyList))
+        await message.channel.send(response)
 
-        
-        if message.content == '.link':
-            response = ""
-            if len(self.readyList) > 1:
-                response  = "Aquí tienes: {}".format(self.pinturillo.URL)
-            await message.channel.send(response)
+    def messageUnready(self, message):
+        """
+        Handles the message if the content was ".unready"
+        """
+        if message.author in self.readyList:
+            self.readyList.remove(message.author)
+            response = "Terrible, {}. Eso es porque tienes miedo...".format(message.author.name)
+        else:
+            response = "Pero si no habías hecho ready, so merluzo."
+        await message.channel.send(response)
 
-        if message.content == '.readyList':            
-            if not self.readyList:
-                response = "No hay nadie todavía."
-            else:
-                response = "Gente ready: {}.".format([person.name for person in self.readyList])
-            await message.channel.send(response)
-            
+    def messageLink(self, message):
+        response = ""
+        if len(self.readyList) > 1:
+            response  = "Aquí tienes: {}".format(self.pinturillo.URL)
+        await message.channel.send(response)
+
+    def messageReadyList(self, message):
+        if not self.readyList:
+            response = "No hay nadie todavía."
+        else:
+            response = "Gente ready: {}.".format([person.name for person in self.readyList])
+        await message.channel.send(response)
 
     def addReady(self, user):
         # if user not in self.readyList:
