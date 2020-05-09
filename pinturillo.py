9c27ec218290347d8cd28097651560fa18223dbc
Sergio Barril <sergiobarrilpizarro@gmail.com>
2020-05-09 22:49:47 +0200

    v1.0.0

--- pinturillo.py
+++ pinturillo.py
@@ -8,19 +8,32 @@ from selenium.webdriver.support.wait import WebDriverWait
 
 
 class Pinturillo():
-    def __init__(self, customs=True, language="Spanish", rounds=4,  drawTime=80, onlyCustoms=False):
+
+    VALID_CONFIG_KEYS = ("minimo", "customs", "language", "rounds", "drawTime", "onlyCustoms")
+    VALID_ROUNDS = [str(i) for i in range(2, 11)]
+    VALID_DRAWTIME = [str(i) for i in range(30, 181, 10)]
+    VALID_LANGUAGES = ("English", "German", "Bulgarian", "Czech", "Danish",
+        "Dutch", "Finnish", "French", "Estonian", "Greek", "Hebrew", "Hungarian",
+        "Italian", "Korean", "Latvian", "Macedonian", "Norwegian", "Portuguese", "Polish", "Romanian",
+        "Serbian", "Slovakian", "Spanish", "Sedish", "Tagalog", "Turkish")
+
+    def __init__(self, customs=False, language="Spanish", rounds=4,  drawTime=80, onlyCustoms=False, roomConfig = None):
         self.driver = webdriver.Chrome('driver/chromedriver.exe')
         
-        self.roomConfig = {
-            "customs" : customs,
-            "language" : language,
-            "rounds"  : rounds,
-            "drawTime" : drawTime,
-            "onlyCustoms" : onlyCustoms,        
-        }
+        if roomConfig is not None:
+            self.roomConfig = roomConfig
+        else:
+            self.roomConfig = {
+                "customs" : customs,
+                "language" : language,
+                "rounds"  : rounds,
+                "drawTime" : drawTime,
+                "onlyCustoms" : onlyCustoms,        
+            }
         
         self.customs = ""
         self.URL = ""
+
 
     def run(self):
         # Open Skribbl.io
@@ -37,7 +50,7 @@ class Pinturillo():
         # Create a Private Room
         privateRoomButton = self.driver.find_element_by_xpath('//*[@id="buttonLoginCreatePrivate"]')
         privateRoomButton.click()
-        sleep(1.5)
+        sleep(2)
 
         # Configuration
         self.roomConfiguration()
@@ -60,20 +73,33 @@ class Pinturillo():
         languageSelector.select_by_visible_text(self.roomConfig['language'])
 
         # Load customs
+        customsInput = self.driver.find_element_by_xpath('//*[@id="lobbySetCustomWords"]')
         if(self.roomConfig['customs']):
             self.readCustoms()
-            customsInput = self.driver.find_element_by_xpath('//*[@id="lobbySetCustomWords"]')
-            customsInput.send_keys(self.customs)
-
-            #Only Custom Words
-            if(self.roomConfig['onlyCustoms']):
-                onlyCustomsBox = self.driver.find_element_by_xpath('//*[@id="lobbyCustomWordsExclusive"]')
-                onlyCustomsBox.click()
+            if customsInput.get_attribute('value') != self.customs:                
+                customsInput.send_keys(self.customs)
+        else:
+            customsInput.clear()
     
+        #Only Custom Words
+        onlyCustomsBox = self.driver.find_element_by_xpath('//*[@id="lobbyCustomWordsExclusive"]')      
+        if(self.roomConfig['onlyCustoms'] != onlyCustomsBox.is_selected()):  
+            onlyCustomsBox.click()
+        
     def readCustoms(self):
         with open('customs.txt', encoding="utf-8") as f:
             self.customs = f.read()
-
+        return self.customs
+    
+    def startGame(self):
+        startGameButton = self.driver.find_element_by_xpath('//*[@id="buttonLobbyPlay"]')
+        startGameButton.click()
+        sleep(0.5)
+        if startGameButton.is_displayed():
+            return False
+        
+        self.driver.quit()
+        return True
 
 if __name__ == "__main__":
     bot = Pinturillo()
--- skribbot.py
+++ skribbot.py
@@ -20,7 +20,16 @@ class Skribbot(discord.Client):
         super().__init__()
         self.pinturillo = None
         self.readyList  = []
-
+        self.roomConfig = {
+            "customs" : True,
+            "language" : "Spanish",
+            "rounds"  : 4,
+            "drawTime" : 80,
+            "onlyCustoms" : False,
+        }
+        self.minimo = 4
+        self.oldURL = ""
+    
     async def on_ready(self):
         guild = discord.utils.get(self.guilds, name=GUILD)
         print(
@@ -32,20 +41,37 @@ class Skribbot(discord.Client):
         if message.author == self.user:
             return
         
+        response = ""
+
         if message.content == '.ready':
-            self.messageReady(message)
+            response = await self.messageReady(message)
 
         elif message.content == '.unready':
-            self.messageUnready(message)
+            response = self.messageUnready(message)
         
         elif message.content == '.link':
-            self.messageLink(message)
+            response = self.messageLink(message)
 
         elif message.content == '.readyList':
-            self.messageReadyList(message)       
+            response = self.messageReadyList(message)
+
+        elif message.content == ".start":
+            response = self.messageStart(message)
+
+        # elif message.content == ".customs":
+        # response = self.getCustoms()
+        
+        elif message.content.startswith(".config"):
+            response = self.messageConfig(message)
+        
+        elif message.content.split(' ')[0][1:] in Pinturillo.VALID_CONFIG_KEYS:
+            response = self.messageShortConfig(message)
+
+        if response:
+            await message.channel.send(response)
                         
 
-    def messageReady(self, message):
+    async def messageReady(self, message):
         """
         Handles the action if the message received was .ready
         """
@@ -53,25 +79,25 @@ class Skribbot(discord.Client):
       
         # FIRST MESSAGE
         if self.pinturillo is None:
-            self.pinturillo = Pinturillo()
-            # self.addReady(message.author)
-            response = "Venga va, ¿quién más se apunta? ¡Ya hay 1/4! @everyone"
+            self.pinturillo = Pinturillo(roomConfig = self.roomConfig)
+            self.addReady(message.author)
+            response = "Venga va, ¿quién más se apunta? ¡Ya hay 1/{}! @everyone".format(self.minimo)
 
         # IF NOT ADDED ALREADY
         elif self.addReady(message.author):
             response = "¡Perfecto, {}!".format(message.author.mention)
 
-            if len(self.readyList) >= 2:
+            if len(self.readyList) >= self.minimo:
                 response += " ¡Somos suficientes para que no dé pena!"
                 if not self.pinturillo.URL:
                     self.pinturillo.URL = "Loading"
                     await message.channel.send("Cargando sala...")
                     self.pinturillo.run()
-                    response += "Aquí tenéis el enlace: {}".format(self.pinturillo.URL)
+                    response += " Aquí tenéis el enlace: {}".format(self.pinturillo.URL)
                 
             else:
-                response += " Ya somos {}/4".format(len(self.readyList))
-        await message.channel.send(response)
+                response += " Ya somos {}/{}".format(len(self.readyList), self.minimo)
+        return response
 
     def messageUnready(self, message):
         """
@@ -82,26 +108,184 @@ class Skribbot(discord.Client):
             response = "Terrible, {}. Eso es porque tienes miedo...".format(message.author.name)
         else:
             response = "Pero si no habías hecho ready, so merluzo."
-        await message.channel.send(response)
+        return response
 
     def messageLink(self, message):
-        response = ""
-        if len(self.readyList) > 1:
+        if self.pinturillo is None or not self.pinturillo.URL:
+            if not self.oldURL:
+                response = f"Aún no he abierto ninguna sala..."
+            else:
+                response = f"La última vez los dejé en {self.oldURL}... ¿Si corres quizá los pillas?"
+
+        if len(self.readyList) >= self.minimo:
             response  = "Aquí tienes: {}".format(self.pinturillo.URL)
-        await message.channel.send(response)
+        return response
 
     def messageReadyList(self, message):
         if not self.readyList:
             response = "No hay nadie todavía."
         else:
-            response = "Gente ready: {}.".format([person.name for person in self.readyList])
-        await message.channel.send(response)
+            readyPeople = [person.name for person in self.readyList]
+            stringPeople = ", ".join(readyPeople)
+            response = "Gente ready: {}.".format(stringPeople)
+        return response
+
+    def messageStart(self, message):
+        if self.pinturillo is None:
+            return "Start qué, si no aún no hay nada preparado... Haz .ready anda"
+        
+        if not self.pinturillo.URL or self.pinturillo.URL == "Loading...":
+            return "Aún no estamos en la sala, cálmate y dale a .ready si no lo has hecho ya"
+        
+        if self.pinturillo.startGame():
+            self.oldURL = self.pinturillo.URL
+            self.pinturillo = None
+            self.readyList = []
+            return"¡A jugarrrr~!"
+        
+        return "Estoy yo solo todavía, espera a que entre la gente"
+
+    def messageConfig(self, message):
+        words = message.content.split(" ")
+        words.remove(".config")
+
+        newConfig = self.roomConfig.copy()
+
+        if not words:
+            return self.getCurrentConfig()
+        
+        for word in words:
+            param = word.lower()
+
+            # Explicit parameters
+            if '=' in param:
+                key, value = param.split("=")
+                self.setConfig(key, value, newConfig)
+
+            # Direct language edit
+            if 'spanish' == param or 'español' == param:
+                newConfig['language'] = 'Spanish'
+            if 'english' == param or 'inglés' == param or 'ingles' == param:
+                newConfig['language'] = 'English'
+            
+            # Direct customs edit
+            if 'customs' == param:
+                newConfig['customs'] = True
+            if 'nocustoms' == param or "sincustoms" == param:
+                newConfig['customs'] = False
+            
+            # Direct onlycustoms edit
+            if 'onlycustoms' == param:
+                newConfig['onlyCustoms'] = True
+            if 'mixto' == param:
+                newConfig['onlyCustoms'] = False
+        
+        self.roomConfig = newConfig
+
+        if self.pinturillo is not None and self.pinturillo.URL:
+            self.pinturillo.roomConfig = self.roomConfig
+            self.pinturillo.roomConfiguration()
+        
+        return "Configuration updated!"
+    
+    def messageShortConfig(self, message):
+        if len(message.content.split(' ')) == 1:
+            key = message.content.split(' ')[0][1:].lower()
+            return self.getParam(key)
+        else:
+            key, value = message.content.split(' ')
+            key = key[1:].lower()
+        
+        isUpdated = self.setConfig(key, value, self.roomConfig)
+
+        if isUpdated:
+            if self.pinturillo is not None and self.pinturillo.URL:
+                self.pinturillo.roomConfig = self.roomConfig
+                self.pinturillo.roomConfiguration()
+            return f'{key.capitalize()} updated to {value}.'
+        
+        return f'{value.capitalize} is not a valid value for {key}'
+
+    def setConfig(self, key, value, newConfig):
+        YES_OPTIONS = ("true", "si", "yes", "sí")
+        NO_OPTIONS = ("false", "no")
+
+        if key == 'language':
+            language = value.capitalize()
+            if language in Pinturillo.VALID_LANGUAGES:
+                newConfig['language'] = language
+                return True
+
+        if key == 'rounds':
+            if value in Pinturillo.VALID_ROUNDS:
+                newConfig['rounds'] = int(value)
+                return True
+        
+        if key == 'customs':
+            isCustoms = value.lower()
+            if isCustoms in YES_OPTIONS:
+                newConfig['customs'] = True
+                return True
+            elif isCustoms in NO_OPTIONS:
+                newConfig['customs'] = False
+                return True
+        
+        if key == 'onlycustoms':
+            isCustoms = value.lower()
+            if isCustoms in YES_OPTIONS:
+                newConfig['onlyCustoms'] = True
+                return True
+            elif isCustoms in NO_OPTIONS:
+                newConfig['onlyCustoms'] = False
+                return True
+        
+        if key == 'drawtime':
+            if value in Pinturillo.VALID_DRAWTIME:
+                newConfig['drawTime'] = int(value)
+                return True
+
+        if key == 'minimo':
+            if value.isdigit():
+                self.minimo = int(value)
+                return True
+
+        
+        return False
+        
+
+    def getCurrentConfig(self):
+        response = "\n**__Current configuration:__**\n"
+        for key, value in self.roomConfig.items():
+            response += f"**{key.capitalize()}**: _{value}_\n"
+        return response
+
+    def getParam(self, key):
+        value = self.minimo if key == 'minimo' else self.roomConfig[key]
+        return f'**{key.capitalize()}:** _{value}_'
+
 
     def addReady(self, user):
-        # if user not in self.readyList:
-        self.readyList.append(user)
-        return True
-        # return False
+        if user not in self.readyList:
+            self.readyList.append(user)
+            return True
+        return False
+    
+    # def getCustoms(self):
+    #     if self.pinturillo.customs:
+    #         customs = self.pinturillo.customs
+    #     elif self.pinturillo:
+    #         customs = self.pinturillo.readCustoms()
+    #     else:
+    #         return []
+        
+    #     if len(customs) < 1999:
+    #         return [f"```{customs}```"]
+        
+    #     list(map(strip, customs.split(',')))
+
+        
+        
+        
 
 client = Skribbot()
 client.run(TOKEN)
